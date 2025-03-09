d1707833e28be39b34522a24beb106acf7b48539
Neyshiru <30559792+Neyshiru@users.noreply.github.com>
2025-03-09 01:31:24 +0100

    Delete MyJavaFXTemplate/doc directory

--- MyJavaFXTemplate/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"controller"},{"l":"model"},{"l":"resources"},{"l":"tests"},{"l":"view"}]
--- MyJavaFXTemplate/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"controller","l":"Controller"},{"p":"view","l":"Main"},{"p":"model","l":"MyModel"},{"p":"tests","l":"MyTest"},{"p":"resources","l":"Resources"}]
